efd6d34218159d42f2865510ea127abbd92ffcc2
Max Kosh <koshelmak@gmail.com>
2024-11-21 22:15:50 +0200

    rewrote the class constructor and count

--- DataStruct.Lib/Kosharr.cs
+++ DataStruct.Lib/Kosharr.cs
@@ -6,17 +6,44 @@ namespace DataStruct.Lib
     {
         //  List<int> df ;
         private int count = 0 ;
-        public Kosharr(params object [] obj ) 
-        {
-            foreach (object o in obj) 
-            { 
-            count++;
-            }
-            object[] _innerArray = new object[count];
-        }
-        public int Count() { return count ; }
-                             //return count > 0 ? count : 0; }
+        private object[] InnerArray;
 
-       // private object[] _innerArray = new object[1]; 
+        public Kosharr(params object[] obj)
+        {
+            InnerArray = obj ;
+        }
+
+        public int Count() {
+            foreach (object o in InnerArray)
+            {
+                count++;
+            }
+            return count;
+        }
+        public object this[int index]
+        {
+            get => InnerArray [index];     // Читання значення за індексом
+            set => InnerArray[index] = value; // Запис значення за індексом
+        }
+        //public Kosharr(params object [] obj ) 
+        //{
+        //    foreach (object o in obj) 
+        //    { 
+        //    count++;
+        //    }
+
+        //   for (int i = 0; i < count; i++) 
+        //    {
+        //        InnerArray[i]=obj[i];
+        //    }
+        //}
+        // object[] _innerArray = new object[count];
+
+
+
+        //public int Count() {  return count ; }
+        //return count > 0 ? count : 0; }
+
+        // private object[] _innerArray = new object[1]; 
     }
 }
--- DataStruct.Tests/Program.cs
+++ DataStruct.Tests/Program.cs
@@ -8,7 +8,10 @@ namespace DataStruct.Tests
         {
             Console.WriteLine("Hello, World!");
             int e = 2;
-            var d = new Kosharr(1,1,2);
+            // int[] mas = new int[3]; 
+            int[] mas = new int[] { 1, 2, 3 };
+            var d = new Kosharr(2,2,2);
+            d[2] = 0;
             Console.WriteLine(Convert.ToString(d.Count()));
            
             //Kosharr d = new Kosharr(e);
