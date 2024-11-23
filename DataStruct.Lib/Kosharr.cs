7cab60ea08fde85db9c2a932872722fa271f5d49
Max Kosh <koshelmak@gmail.com>
2024-11-24 00:45:39 +0200

    Add(object) Insert(int, object) Remove(object) RemoveAt(int) Clear() bool Contains(object) int IndexOf(object)  object[] ToArray() Reverse()

--- DataStruct.Lib/Kosharr.cs
+++ DataStruct.Lib/Kosharr.cs
@@ -7,15 +7,17 @@ namespace DataStruct.Lib
     {
 
         private object[] _innerArray;
-        public int count { get; private set; }
-        public int countR { get; private set; }
+        public int _count { get; private set; }
+        // public int countR { get; private set; }
+
         public Kosharr(params object[] obj)
         {
             _innerArray = obj;
             foreach (object o in _innerArray)
             {
-                count++;
+                _count++;
             }
+            //countR = count;
         }
 
 
@@ -27,14 +29,15 @@ namespace DataStruct.Lib
 
         public void Add(object obj)
         {
-            object[] TempInnerArray = new object[count + 1];
-            for (int i = 0; i < count; i++)
+            object[] TempInnerArray = new object[_count + 1];
+            for (int i = 0; i < _count; i++)
             {
                 TempInnerArray[i] = _innerArray[i];
             }
-            TempInnerArray[count] = obj;
+            TempInnerArray[_count] = obj;
             _innerArray = TempInnerArray;
-            count++;
+            _count++;
+
         }
 
         public void Insert(int num, object obj)
@@ -42,28 +45,26 @@ namespace DataStruct.Lib
             _innerArray[num - 1] = obj;
         }
 
-        public bool RemoveAt(int num)
-        {
-            return true;
-        } 
+
         public bool Remove(object obj)
         {
             bool rem = false;
-            object[] tempInnerArray = new object[count];
+            int countR = _count;
+            object[] tempInnerArray = new object[_count];
             int index = -1;
-            countR = count - 1;
-            foreach (object o in _innerArray) 
+            countR = _count - 1;
+            foreach (object o in _innerArray)
             {
                 index++;
-                if (o.Equals(obj)&& rem==false)
+                if (o.Equals(obj) && rem == false)
                 {
-                    
+
                     rem = true;
                     break;
                 }
-              
+
             }
-            for (int i = 0;i< countR; i++)
+            for (int i = 0; i < countR; i++)
             {
                 if (index == i && rem == true)
                 {
@@ -71,24 +72,111 @@ namespace DataStruct.Lib
                 }
                 else
                 {
-                    if (i< index)
+                    if (i < index)
                     {
                         tempInnerArray[i] = _innerArray[i];
                     }
                     else
                     {
-                        tempInnerArray[i] = _innerArray[i+1];
-                    } 
+                        tempInnerArray[i] = _innerArray[i + 1];
+                    }
 
                 }
-                
+
             }
 
-            countR = count - countR;
-            count = count - countR;
-            countR = count + countR; 
+            countR = _count - countR;
+            _count = _count - countR;
+            countR = _count + countR;
             _innerArray = tempInnerArray;
             return rem;
+        }
+
+        public bool RemoveAt(int num)
+        {
+            bool rem = false;
+            int countR = _count;
+            object[] tempInnerArray = new object[_count];
+            int index = num - 1;
+            countR = _count - 1;
+            for (int i = 0; i < countR; i++)
+            {
+                if (index == i && rem == false)
+                {
+                    tempInnerArray[i] = _innerArray[i + 1];
+                    rem = true;
+                }
+                else
+                {
+                    if (i < index)
+                    {
+                        tempInnerArray[i] = _innerArray[i];
+                    }
+                    else
+                    {
+                        tempInnerArray[i] = _innerArray[i + 1];
+                    }
+
+                }
+
+            }
+
+            countR = _count - countR;
+            _count = _count - countR;
+            countR = _count + countR;
+            _innerArray = tempInnerArray;
+            return rem;
+        }
+
+        void Clear()
+        {
+            for (int i = 0; i < _count; i++)
+            {
+                _innerArray[i] = null;
+            }
+
+            _count = 0;
+        }
+
+        public bool Contains(object obj)
+        {
+            foreach (object ob in _innerArray)
+            {
+                if (ob.Equals(obj)) return true;
+            }
+            return false;
+        }
+
+        public int IndexOf(object obj)
+        {
+            int index = -1;
+            foreach (object ob in _innerArray)
+            {
+                index++;
+                if (ob.Equals(obj)) return index;
+            }
+            return -1;
+        }
+
+        public object [] ToArray()
+        {
+          
+            return _innerArray;
+        }
+
+        public void Reverse() 
+        {
+            int left = 0;
+            int right = _count - 1;
+            while (left < right)
+            {
+                object temp = _innerArray[left];
+                _innerArray[left] = _innerArray[right];
+                _innerArray[right] = temp;
+
+                left++;
+                right--;
+            }
         }
 
     }
--- DataStruct.Tests/Program.cs
+++ DataStruct.Tests/Program.cs
@@ -10,19 +10,31 @@ namespace DataStruct.Tests
             int e = 2;
             // int[] mas = new int[3]; 
             int[] mas = new int[] { 1, 2, 3 };
-            var d = new Kosharr(1, 3, 2, 2,2,2, 4);
+            var d = new Kosharr(1, 3,2, 2,5, 4);
             //   d[2] = 0;
             // d.Add(1);
             // d.Insert(1, 5);
-            for (int i = 0; i < d.count; i++)
+            //int [] array = d.ToArray();
+            for (int i = 0; i < d._count; i++)
             {
                 Console.Write($"{d[i]} ");
             }
             Console.WriteLine();
             // Console.WriteLine(Convert.ToString(d[1]));
+          bool s=  d.Contains(9);
+            int t = d.IndexOf(2);
             int j = 2;
-            d.Remove(j);
-            for (int i = 0; i < d.count; i++)
+
+            // d.Remove(1);
+            d.Reverse();
+            for (int i = 0; i < d._count; i++)
+            {
+                Console.Write($"{d[i]} ");
+            }
+            Console.WriteLine();
+         //   d.RemoveAt(4);
+            d.Reverse();
+            for (int i = 0; i < d._count; i++)
             {
                 Console.Write($"{d[i]} ");
             }
